2e727a36f0923173851ac3c341a58d08bd729651
Daniel Kliewer <danielkliewer@gmail.com>
2026-07-09 13:07:06 -0500

    debug: add logging to GA4 component

--- src/components/layout/GoogleAnalytics.tsx
+++ src/components/layout/GoogleAnalytics.tsx
@@ -6,6 +6,10 @@ const GA_MEASUREMENT_ID = 'G-02N9FT7XP5';
 
 export function GoogleAnalytics() {
   useEffect(() => {
+    console.log('[GA4] GoogleAnalytics useEffect running');
+    console.log('[GA4] window.gtag exists:', !!window.gtag);
+    console.log('[GA4] window.dataLayer:', window.dataLayer?.length, 'entries');
+
     window.dataLayer = window.dataLayer || [];
     if (!window.gtag) {
       function gtag(...args: unknown[]) {
@@ -14,10 +18,13 @@ export function GoogleAnalytics() {
       window.gtag = gtag;
     }
 
+    console.log('[GA4] calling gtag js');
     window.gtag!('js', new Date());
+    console.log('[GA4] calling gtag config');
     window.gtag!('config', GA_MEASUREMENT_ID, {
       page_path: window.location.pathname,
     });
+    console.log('[GA4] dataLayer now has', window.dataLayer.length, 'entries');
   }, []);
 
   return null;
