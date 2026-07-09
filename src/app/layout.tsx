cb52281a6811c9d289899565a6b6e04b44880096
Daniel Kliewer <danielkliewer@gmail.com>
2026-07-09 12:26:18 -0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -72,6 +72,17 @@ export default function RootLayout({
         <Script src="//cdn.iubenda.com/cs/gpp/stub.js" strategy="beforeInteractive" />
         <Script src="//cdn.iubenda.com/cs/iubenda_cs.js" strategy="beforeInteractive" />
         <Script src="https://www.googletagmanager.com/gtag/js?id=G-02N9FT7XP5" strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-02N9FT7XP5', {
+              page_path: window.location.pathname,
+            });
+          `}
+        </Script>
         <Script src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5368889366103187" crossOrigin="anonymous" strategy="afterInteractive" />
         <Script id="theme-detection" strategy="beforeInteractive">
           {`(function() {
